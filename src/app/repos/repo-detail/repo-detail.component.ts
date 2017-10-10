adee28c3b9fec599435447e8cb16ee399f506456
Gabriel Papke <gabrielpapke@gmail.com>
2017-10-09 21:54:25 -0300

    RepoDetail Component template

--- src/app/repos/repo-detail/repo-detail.component.ts
+++ src/app/repos/repo-detail/repo-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from "@angular/common";
+
 
 @Component({
   selector: 'app-repo-detail',
@@ -7,9 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RepoDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(private location: Location) { }
 
   ngOnInit() {
   }
-
+  public goBack() {
+    this.location.back();
+  }
 }
